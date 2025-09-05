1a6c5b87e77e9d27fcf5fc30d130fd5c9983fe8a
NeavJain <135718395+NeavJain@users.noreply.github.com>
2025-09-05 15:57:28 +0530

    Delete question_4.py

--- question_4.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-test = np.random.randint(1,100,size=(5,5))
-print(test)
-print(test[1:4 , 1:4])    #only access rows n colmns from 1 to 3 
-print(test[0,0:])
-print(test[0: , 4])
-print(np.dot(test[0,0:],test[0: , 4]))
